f173b390ebfe072cc347cd1a119ec90a5de5a393
zhenghao <zhenghao@cmss.chinamobile.com>
2018-11-07 23:30:08 +0800

    引入redis存储
    redis存储

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,8 @@
 const router = require('koa-router')()
 const Person = require('../dbs/modules/person')
+const Redis = require('koa-redis')
+
+const Store = new Redis().client // 创建一个redis的客户端实例
 
 router.prefix('/users')
 
@@ -65,6 +68,18 @@ router.post('/removePerson', async function (ctx) {
         name: ctx.request.body.name
     }).remove()
 
+    ctx.body = {
+        code: 0
+    }
+})
+
+router.get('/fix', async function (ctx) {
+    // 不通过session，直接存储redis数据
+    /* 在redis cli中查看变量
+    * 1. keys *
+    * 2. hget fix name 获取值
+    * */
+    const st = await Store.hset('fix', 'name', Math.random())
     ctx.body = {
         code: 0
     }
